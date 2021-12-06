b8dc74b7120495a26af1b5a720f6fd4f5e5f1eec
Admin <Admin@507-4>
2021-12-06 16:49:38 +0300

    Добавил кнопки

---
+++ Zadanie_1_2/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Zadanie_1_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnAW_BLEND = new System.Windows.Forms.Button();
+            this.btnHOR_AW_SLIDE = new System.Windows.Forms.Button();
+            this.btnCenter_AW_SLIDE = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAW_BLEND
+            // 
+            resources.ApplyResources(this.btnAW_BLEND, "btnAW_BLEND");
+            this.btnAW_BLEND.Name = "btnAW_BLEND";
+            this.btnAW_BLEND.UseVisualStyleBackColor = true;
+            this.btnAW_BLEND.Click += new System.EventHandler(this.btnAW_BLEND_Click);
+            // 
+            // btnHOR_AW_SLIDE
+            // 
+            resources.ApplyResources(this.btnHOR_AW_SLIDE, "btnHOR_AW_SLIDE");
+            this.btnHOR_AW_SLIDE.Name = "btnHOR_AW_SLIDE";
+            this.btnHOR_AW_SLIDE.UseVisualStyleBackColor = true;
+            this.btnHOR_AW_SLIDE.Click += new System.EventHandler(this.btnHOR_AW_SLIDE_Click);
+            // 
+            // btnCenter_AW_SLIDE
+            // 
+            resources.ApplyResources(this.btnCenter_AW_SLIDE, "btnCenter_AW_SLIDE");
+            this.btnCenter_AW_SLIDE.Name = "btnCenter_AW_SLIDE";
+            this.btnCenter_AW_SLIDE.UseVisualStyleBackColor = true;
+            this.btnCenter_AW_SLIDE.Click += new System.EventHandler(this.btnCenter_AW_SLIDE_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnCenter_AW_SLIDE);
+            this.Controls.Add(this.btnHOR_AW_SLIDE);
+            this.Controls.Add(this.btnAW_BLEND);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAW_BLEND;
+        private System.Windows.Forms.Button btnHOR_AW_SLIDE;
+        private System.Windows.Forms.Button btnCenter_AW_SLIDE;
+    }
+}
+
