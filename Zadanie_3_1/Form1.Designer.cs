083cf3818317b54a2d5e2509c0e0096677b2df76
Admin <Admin@507-4>
2021-12-07 10:22:30 +0300

    Добавил свойство

---
+++ Zadanie_3_1/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Zadanie_3_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txbB = new System.Windows.Forms.TextBox();
+            this.txbA = new System.Windows.Forms.TextBox();
+            this.btnWork = new System.Windows.Forms.Button();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.lblB = new System.Windows.Forms.Label();
+            this.lblA = new System.Windows.Forms.Label();
+            this.helpProvider1 = new System.Windows.Forms.HelpProvider();
+            this.SuspendLayout();
+            // 
+            // txbB
+            // 
+            this.txbB.Location = new System.Drawing.Point(291, 21);
+            this.txbB.Name = "txbB";
+            this.txbB.Size = new System.Drawing.Size(100, 20);
+            this.txbB.TabIndex = 11;
+            // 
+            // txbA
+            // 
+            this.helpProvider1.SetHelpString(this.txbA, "For input integer A");
+            this.txbA.Location = new System.Drawing.Point(92, 21);
+            this.txbA.Name = "txbA";
+            this.helpProvider1.SetShowHelp(this.txbA, true);
+            this.txbA.Size = new System.Drawing.Size(100, 20);
+            this.txbA.TabIndex = 10;
+            // 
+            // btnWork
+            // 
+            this.btnWork.Location = new System.Drawing.Point(101, 61);
+            this.btnWork.Name = "btnWork";
+            this.btnWork.Size = new System.Drawing.Size(75, 23);
+            this.btnWork.TabIndex = 9;
+            this.btnWork.Text = "Работа";
+            this.btnWork.UseVisualStyleBackColor = true;
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(20, 61);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(75, 23);
+            this.btnRun.TabIndex = 8;
+            this.btnRun.Text = "Сумма";
+            this.btnRun.UseVisualStyleBackColor = true;
+            // 
+            // lblB
+            // 
+            this.lblB.AutoSize = true;
+            this.lblB.Location = new System.Drawing.Point(220, 21);
+            this.lblB.Name = "lblB";
+            this.lblB.Size = new System.Drawing.Size(65, 13);
+            this.lblB.TabIndex = 7;
+            this.lblB.Text = "Значение В";
+            // 
+            // lblA
+            // 
+            this.lblA.AutoSize = true;
+            this.lblA.Location = new System.Drawing.Point(12, 21);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(65, 13);
+            this.lblA.TabIndex = 6;
+            this.lblA.Text = "Значение А";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(419, 123);
+            this.Controls.Add(this.txbB);
+            this.Controls.Add(this.txbA);
+            this.Controls.Add(this.btnWork);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.lblB);
+            this.Controls.Add(this.lblA);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txbB;
+        private System.Windows.Forms.TextBox txbA;
+        private System.Windows.Forms.Button btnWork;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.Label lblB;
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.HelpProvider helpProvider1;
+    }
+}
+
