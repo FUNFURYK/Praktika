0d1c63150eb944a53f574005f5bf2325bd2a350a
Admin <Admin@507-4>
2021-12-07 09:10:27 +0300

    Добавил 2 button, 2 txb, 2 lbl

---
+++ Zadanie_2_3/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zadanie_2_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+
+    private delegate int AsyncSumm(int a, int b);
+    private int Summ(int a, int b)
+    {
+        System.Threading.Thread.Sleep(9000);
+        return a + b;
+    }
+}
